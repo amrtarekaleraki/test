c46e022896debc6b7fab2112f785eee9ae5c0d76
amrtarekaleraki <104483594+amrtarekaleraki@users.noreply.github.com>
2022-06-09 15:43:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+  
+    // Standard alert
+    $('.alert').on('click', function() {
+            alert('سيتم إصدار التطبيق قريبا')
+    });
+
+});
+
+
+// function clickme()
+// {
+//     swal("مرحبا", "سيتم إصدار التطبيق قريبا", "success");
+      
+// }
+
+
+// document.querySelector(".first").addEventListener('click', function(){
+//     Swal.fire({
+//         title: "Alert Set on Timer",
+//         timer: 10000
+//       });
+//   });
